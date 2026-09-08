ab549f013fa684b3f3b98b24585b1d98478826a1
ZeroLevel <zeroleveldev@outlook.com>
2026-09-07 18:40:41 -0700

    deploy: connect live Gemini worker

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4170297474" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1685738951" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
