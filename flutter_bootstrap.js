ca4337033cfc863dc950098bd2506eed995f5475
ZeroLevel <zeroleveldev@outlook.com>
2026-09-09 12:46:18 -0700

    deploy: surface beta access request

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2812344452" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2001663098" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
