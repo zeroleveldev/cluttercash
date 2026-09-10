feccd2f059ea242378b4daa8b26f8f889d64c9b9
ZeroLevel <zeroleveldev@outlook.com>
2026-09-10 05:13:46 -0700

    deploy: private in-app beta approval

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2952300014" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4157756274" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
